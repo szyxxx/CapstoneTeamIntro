a0c1edc32e51f71ba93c8283f1467c9de565341c
Axel David <axeldavid1521@gmail.com>
2024-10-07 14:53:18 +0700

    add: valerie to team member

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cardData = [
-        { name: "Axel", description: "Apa aja asal sopan, pasti bisa", imgSrc: "src/asset/axel.png", imgSrcAfter: "src/asset/axel_after.png", linkedin: "https://linkedin.com/in/axelldavid", github: "https://github.com/szyxxx", bg: "#4FACB3", role: "Cloud Computing", univ: "Telkom University" },
-        { name: "Farhan", description: "bisaa amann azaa", imgSrc: "src/asset/farhan.png", imgSrcAfter: "src/asset/farhan_after.png", linkedin: "https://www.linkedin.com/in/farhanrizkifauzi/", github: "", bg: "#4FACB3", role: "Cloud Computing", univ: "Telkom University" },
-        { name: "Tsani", description: "pengendali eyay", imgSrc: "src/asset/tsani.png", imgSrcAfter: "src/asset/tsani_after.png", linkedin: "https://www.linkedin.com/in/m-tsani-faishal-azhar-30846a258/", github: "", bg: "#D3B16C", role: "Machine Learning", univ: "Telkom University" },
-        { name: "Andhin", description: "jurusan MTK bjir", imgSrc: "src/asset/andhin.png", imgSrcAfter: "src/asset/andhin_after.png", linkedin: "https://www.linkedin.com/in/andhin-vaniza-zahranie/", github: "", bg: "#D3B16C", role: "Machine Learning", univ: "Universitas Brawijaya" },
-        { name: "Risa", description: "jurusan MTK bjir(2)", imgSrc: "src/asset/febri.png", imgSrcAfter: "src/asset/febri_after.png", linkedin: "https://www.linkedin.com/in/febrisa-eka-n-patricia-64876b329/", github: "", bg: "#D3B16C", role: "Machine Learning", univ: "Universitas Brawijaya" },
-        // { name: "??", description: "", imgSrc: "src/asset/smith.png", imgSrcAfter: "src/asset/smith_after.png", linkedin: "https://linkedin.com/in/unknown", github: "https://github.com/unknown", bg: "#62BE86", role: "Mobile Development", univ: "" },
+        { name: "Axel", description: '"Apa aja asal sopan, pasti bisa"', imgSrc: "src/asset/axel.png", imgSrcAfter: "src/asset/axel_after.png", linkedin: "https://linkedin.com/in/axelldavid", github: "https://github.com/szyxxx", bg: "#4FACB3", role: "Cloud Computing", univ: "Telkom University" },
+        { name: "Farhan", description: '"bisaa amann azaa"', imgSrc: "src/asset/farhan.png", imgSrcAfter: "src/asset/farhan_after.png", linkedin: "https://www.linkedin.com/in/farhanrizkifauzi/", github: "", bg: "#4FACB3", role: "Cloud Computing", univ: "Telkom University" },
+        { name: "Tsani", description: '"aku pengendali eyay"', imgSrc: "src/asset/tsani.png", imgSrcAfter: "src/asset/tsani_after.png", linkedin: "https://www.linkedin.com/in/m-tsani-faishal-azhar-30846a258/", github: "", bg: "#D3B16C", role: "Machine Learning", univ: "Telkom University" },
+        { name: "Andhin", description: '"respect wok"', imgSrc: "src/asset/andhin.png", imgSrcAfter: "src/asset/andhin_after.png", linkedin: "https://www.linkedin.com/in/andhin-vaniza-zahranie/", github: "", bg: "#D3B16C", role: "Machine Learning", univ: "Universitas Brawijaya" },
+        { name: "Risa", description: '"jurusan MTK bjir"', imgSrc: "src/asset/febri.png", imgSrcAfter: "src/asset/febri_after.png", linkedin: "https://www.linkedin.com/in/febrisa-eka-n-patricia-64876b329/", github: "", bg: "#D3B16C", role: "Machine Learning", univ: "Universitas Brawijaya" },
+        { name: "Valerie", description: "", imgSrc: "src/asset/valerie.png", imgSrcAfter: "src/asset/valerie_after.png", linkedin: "https://linkedin.com/in/valerie-audry-057109216/", github: "https://github.com/ValerieAudry198", bg: "#62BE86", role: "Mobile Development", univ: "Universitas Pancasila" },
         // { name: "??", description: "", imgSrc: "src/asset/emily.png", imgSrcAfter: "src/asset/emily_after.png", linkedin: "https://linkedin.com/in/unknown", github: "https://github.com/unknown", bg: "#62BE86", role: "Mobile Development", univ: "" },
     ];
 
@@ -25,9 +25,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
         cardElement.innerHTML = `
             <div class="team-sec flex flex-col py-0 px-0 items-center self-stretch rounded-3xl" style="flex: 1 0 0; background-color: ${card.bg}; border: 2px solid transparent;">
-                <div class="team-content flex h-[13.56rem] pt-5 flex-col justify-between items-center self-stretch text-white text-center" style="font-family:'Poppins', system-ui;">
+                <div class="team-content flex h-[13.56rem] pt-5 flex-col justify-evenly items-center self-stretch text-white text-center" style="font-family:'Poppins', system-ui;">
                     <span style="font-weight: 600; font-size: 1.8rem">${card.name}</span>
-                    <p style="font-weight: 300; font-size: 1.3rem; width:85%; font-style: italic;">${card.description}</p>
+                    <p style="font-weight: 300; font-size: 1.1rem; width:85%; font-style: italic;">${card.description}</p>
                     <div class="flex justify-center items-center gap-2 self-stretch icons">
                         <a href="${card.linkedin}" target="_blank"><img style="width: 30px;" src="src/asset/linkedin.png" alt="LinkedIn"></a>
                         <a href="${card.github}" target="_blank"><img style="width: 30px;" src="src/asset/github.png" alt="GitHub"></a>
